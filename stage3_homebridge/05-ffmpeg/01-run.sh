d8b325d082415d604c04f510f132b86ba6c65f75
oznu <dev@oz.nu>
2020-06-19 10:34:17 +1000

    use latest ffmpeg

--- stage3_homebridge/05-ffmpeg/01-run.sh
+++ stage3_homebridge/05-ffmpeg/01-run.sh
@@ -11,7 +11,7 @@ uname -a
 set -e
 set -x
 
-wget "https://github.com/homebridge/ffmpeg-for-homebridge/releases/download/v0.0.1/ffmpeg-raspbian-armv6l.tar.gz"
+wget "https://github.com/homebridge/ffmpeg-for-homebridge/releases/latest/download/ffmpeg-raspbian-armv6l.tar.gz"
 tar xzf "ffmpeg-raspbian-armv6l.tar.gz" -C / --no-same-owner
 rm -rf ffmpeg-raspbian-armv6l.tar.gz
 
