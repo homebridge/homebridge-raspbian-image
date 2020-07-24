d8aae18328bd4f5cf9cf4580c5ec058b3d5069bd
Sten Spans <sten@sp5.nl>
2020-07-24 14:17:56 +0200

    Update 01-run.sh

--- stage3_homebridge/05-ffmpeg/01-run.sh
+++ stage3_homebridge/05-ffmpeg/01-run.sh
@@ -11,9 +11,16 @@ uname -a
 set -e
 set -x
 
-wget "https://github.com/homebridge/ffmpeg-for-homebridge/releases/latest/download/ffmpeg-raspbian-armv6l.tar.gz"
-tar xzf "ffmpeg-raspbian-armv6l.tar.gz" -C / --no-same-owner
-rm -rf ffmpeg-raspbian-armv6l.tar.gz
+case "$(dpkg-architecture -q DEB_TARGET_ARCH)" in
+arm64)
+  curl -Lsf "https://github.com/homebridge/ffmpeg-for-homebridge/releases/latest/download/ffmpeg-debian-aarch64.tar.gz" | \
+    tar xf - -C / --no-same-owner
+  ;;
+armhf)
+  curl -Lsf  "https://github.com/homebridge/ffmpeg-for-homebridge/releases/latest/download/ffmpeg-raspbian-armv6l.tar.gz" | \
+    tar xf - -C / --no-same-owner
+  ;;
+esac
 
 ffmpeg || exit 0
 EOF
