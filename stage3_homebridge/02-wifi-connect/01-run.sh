a7a54e1a9e8073d05839e80da9cede514da654b0
oznu <dev@oz.nu>
2020-11-12 23:25:14 +1100

    ensure wifi powersave is really off

--- stage3_homebridge/02-wifi-connect/01-run.sh
+++ stage3_homebridge/02-wifi-connect/01-run.sh
@@ -8,6 +8,7 @@ install -m 644 files/wifi-connect.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/wifi-connect-startup "${ROOTFS_DIR}/usr/local/sbin/"
 install -m 644 files/raspbian-install.sh "${ROOTFS_DIR}/"
 install -m 755 files/log-iface-events.sh "${ROOTFS_DIR}/etc/NetworkManager/dispatcher.d/"
+install -m 644 files/wifi-powersave-off.conf "${ROOTFS_DIR}/etc/NetworkManager/conf.d/"
 
 on_chroot << EOF
 set -x 
