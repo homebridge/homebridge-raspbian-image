c0167c2acb566922a15b64b6e7384463beca7192
oznu <dev@oz.nu>
2021-12-12 17:55:24 +1100

    homebridge wifi connect

--- stage3_homebridge/02-wifi-connect/01-run.sh
+++ stage3_homebridge/02-wifi-connect/01-run.sh
@@ -5,7 +5,6 @@
 #
 
 install -m 644 files/wifi-connect.service "${ROOTFS_DIR}/etc/systemd/system/"
-install -m 644 files/wifi-connect-startup "${ROOTFS_DIR}/usr/local/sbin/"
 install -m 644 files/raspbian-install.sh "${ROOTFS_DIR}/"
 install -m 755 files/log-iface-events.sh "${ROOTFS_DIR}/etc/NetworkManager/dispatcher.d/"
 install -m 644 files/wifi-powersave-off.conf "${ROOTFS_DIR}/etc/NetworkManager/conf.d/"
@@ -13,15 +12,13 @@ install -m 644 files/wifi-powersave-off.conf "${ROOTFS_DIR}/etc/NetworkManager/c
 on_chroot << EOF
 set -x 
 
-chmod u+x /raspbian-install.sh
-sudo /raspbian-install.sh -y
-rm -rf /raspbian-install.sh
-
-chmod +x /usr/local/sbin/wifi-connect-startup
+npm install -g @homebridge/wifi-connect
 
 systemctl daemon-reload
 systemctl enable wifi-connect
 systemctl enable NetworkManager
 systemctl disable dhcpcd
+systemctl disable dnsmasq
+systemctl disable hostapd
 EOF
 
