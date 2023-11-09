7ef201e5f23df41ce744422cc6fca488a92c1e04
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2023-11-09 17:08:51 -0500

    Update ffmpeg version

--- stage3_homebridge/05-ffmpeg/01-run.sh
+++ stage3_homebridge/05-ffmpeg/01-run.sh
@@ -11,9 +11,9 @@ uname -a
 set -e
 set -x
 
-wget "https://github.com/homebridge/ffmpeg-for-homebridge/releases/latest/download/ffmpeg-raspbian-armv6l.tar.gz"
-tar xzf "ffmpeg-raspbian-armv6l.tar.gz" -C / --no-same-owner
-rm -rf ffmpeg-raspbian-armv6l.tar.gz
+wget "https://github.com/homebridge/ffmpeg-for-homebridge/releases/latest/download/ffmpeg-raspbian-arm32v7.tar.gz"
+tar xzf "ffmpeg-raspbian-arm32v7.tar.gz" -C / --no-same-owner
+rm -rf ffmpeg-raspbian-arm32v7.tar.gz
 
 ffmpeg || exit 0
 EOF
