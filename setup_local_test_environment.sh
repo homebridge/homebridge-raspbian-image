5e6b50fbef6bbc70adf1628b4e2034ffaf07b64e
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2025-10-08 18:45:16 -0400

    Update setup_local_test_environment.sh

--- setup_local_test_environment.sh
+++ setup_local_test_environment.sh
@@ -9,17 +9,23 @@ echo
 echo "Cloning recent pi-gen arm64 branch for local testing of build process"
 echo
 
+rm -rf build
+mkdir build && cd build
+
+
 git clone https://github.com/RPi-Distro/pi-gen
 cd pi-gen
-git switch arm64
+git switch bookworm-arm64
 cd ..
 
 echo
 echo "Moving clone for use"
 echo
 
-for i in Dockerfile build.sh build-docker.sh docker-compose.yml export-image export-noobs imagetool.sh scripts stage0 stage1 stage2 stage3 stage4 stage5
-do 
-  echo "Moving "$i
-  mv pi-gen/$i .
-done
+cp ../config ./pi-gen
+cp -r ../stage* ./pi-gen
+#for i in Dockerfile build.sh build-docker.sh docker-compose.yml export-image export-noobs imagetool.sh scripts stage0 stage1 stage2 stage3 stage4 stage5
+#do 
+#  echo "Moving "$i
+#  mv pi-gen/$i .
+#done
