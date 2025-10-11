ca2bdb8ea2b3b8aab56e0f0ff7eaf5ffe583f00d
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2025-10-10 20:07:48 -0400

    Update setup_local_test_environment.sh

--- setup_local_test_environment.sh
+++ setup_local_test_environment.sh
@@ -1,31 +1,22 @@
 #! /bin/sh
 
-echo
-echo "Cleaning up previous pi-gen clone"
-echo
+set -e
 
-rm -rf pi-gen Dockerfile build.sh build-docker.sh docker-compose.yml export-image export-noobs imagetool.sh scripts stage0 stage1 stage2 stage3 stage4 stage5
-echo
-echo "Cloning recent pi-gen arm64 branch for local testing of build process"
-echo
-
-rm -rf build
-mkdir build && cd build
-
+rm -rf pi-gen
 
 git clone https://github.com/RPi-Distro/pi-gen
 cd pi-gen
-git switch bookworm-arm64
+git switch arm64
 cd ..
 
 echo
-echo "Moving clone for use"
+echo "Copying setup for use"
 echo
 
-cp ../config ./pi-gen
-cp -r ../stage* ./pi-gen
-#for i in Dockerfile build.sh build-docker.sh docker-compose.yml export-image export-noobs imagetool.sh scripts stage0 stage1 stage2 stage3 stage4 stage5
-#do 
-#  echo "Moving "$i
-#  mv pi-gen/$i .
-#done
+cp config ./pi-gen
+cp -r stage* ./pi-gen
+
+echo 'To build the image, run:'
+echo
+echo '  docker rm -v pigen_work ; ./pi-gen/build-docker.sh'
+echo
