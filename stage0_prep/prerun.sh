f225d7f30c1e0697021853b72a3798e70a287bca
NorthernMan54 <19808920+NorthernMan54@users.noreply.github.com>
2025-12-01 20:41:54 -0500

    Update prerun.sh

--- stage0_prep/prerun.sh
+++ stage0_prep/prerun.sh
@@ -2,3 +2,10 @@
 
 # Homebridge Image does not need Stage 2 export files
 rm -f ../stage[2]/EXPORT*
+
+ls -lR ../..
+
+echo "Copying raspberrypi.gpg to pi-gen stage0/files"
+echo "Workaround for https://github.com/RPi-Distro/pi-gen/issues/862"
+# Copy raspberrypi.gpg to pi-gen stage0/files
+cp files/raspberrypi.gpg ../pi-gen/stage0/files/raspberrypi.gpg
